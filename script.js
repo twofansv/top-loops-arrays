c704350c66b2d526a81f457bc892675fd8c1cb25
twofansv <95734976+twofansv@users.noreply.github.com>
2026-01-01 16:03:52 +0800

    [+] Continued to the lesson up until the last code.

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const FAMILY = ["Stella", "France", "Evo", "Evie"];
+
+
+for (const fam of FAMILY){
+    console.log(fam);
+}
+
+
+function toUpper(string){
+    return string.toUpperCase();
+}
+
+const upperFam = FAMILY.map(toUpper);
+
+
+console.log(upperFam);
+
+function lFam(fam){
+    return fam.startsWith("E");
+}
+
+const filtered = FAMILY.filter(lFam);
+
+console.log(filtered);
